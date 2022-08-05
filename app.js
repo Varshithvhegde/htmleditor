db73d9501539897eba3e88bd573370ace2c8bb45
Varshithvhegde <varshithvh@gmail.com>
2022-08-05 19:23:28 +0530

    Cleared the issue that was backspace not woeking for autocompleting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-
-function update() {
+var j=0;
+function update(i) {
+	if(i==0){
 	let htmlCode=document.getElementById("htmlCode").value;
 	let cssCode=document.getElementById("cssCode").value;
 	let javascriptCode=document.getElementById("javascriptCode").value;
@@ -9,6 +10,15 @@ function update() {
 	iframe.open();
 	iframe.write(text);
 	iframe.close();
+	}
+	else if(i==1){
+		
+		let htmlCode=document.getElementById("htmlCode").value;
+		let html=htmlCode.slice(0,htmlCode.length);
+		document.getElementById("htmlCode").value=html;
+		j=1;
+
+	}
 	
 }
 const closeChars = new Map([
@@ -17,14 +27,14 @@ const closeChars = new Map([
 	['(', ')'],
 	['<','>'],
 	['"','"'],
-	["'","'"],
-	['<','>']
+	["'","'"]
+	
   ]);
 	  
   htmlCode=document.getElementById('htmlCode');
   
   htmlCode.addEventListener('input', function (e) {
-  
+  if(j!=1){
 	  const pos = e.target.selectionStart;
 	  const val = [...e.target.value];
 	  
@@ -36,6 +46,8 @@ const closeChars = new Map([
 		e.target.value = val.join('');
 		e.target.selectionEnd = pos;
 	  }
+	}
+	j=0;
   });
 
   cssCode=document.getElementById('cssCode');
@@ -70,40 +82,42 @@ const closeChars = new Map([
 		e.target.value = val.join('');
 		e.target.selectionEnd = pos;
 	  }
+	  
   });
 
+ 
 
 
-function BuiltInElements(id){
-	let htmlCode=document.getElementById("htmlCode").value;
-	if(id=="h"){
-	if(htmlCode.length==0){
-		htmlCode+="<h1></h1>";
-	}
-	else{
-	htmlCode+="\n"+"<h1></h1>";
-	}
-	}
-	if(id=="image"){
-		if(htmlCode.length==0){
-			htmlCode+="<img src='' alt='add image'>";
-		}else{
-			htmlCode+="\n"+"<img src='' alt='add image'>";
-		}
-	}
-	if(id=='form'){
+// function BuiltInElements(id){
+// 	let htmlCode=document.getElementById("htmlCode").value;
+// 	if(id=="h"){
+// 	if(htmlCode.length==0){
+// 		htmlCode+="<h1></h1>";
+// 	}
+// 	else{
+// 	htmlCode+="\n"+"<h1></h1>";
+// 	}
+// 	}
+// 	if(id=="image"){
+// 		if(htmlCode.length==0){
+// 			htmlCode+="<img src='' alt='add image'>";
+// 		}else{
+// 			htmlCode+="\n"+"<img src='' alt='add image'>";
+// 		}
+// 	}
+// 	if(id=='form'){
 
-		if(htmlCode.length==0){
-			htmlCode+="<form>"+"\n"+"<input type='text'>"+"\n"+"<input type='submit'>"+"\n"+"</form>";
-		}
-		else{
-			htmlCode+="\n"+"<form>"+"\n"+"<input type='text'>"+"\n"+"<input type='submit'>"+"\n"+"</form>";
-		}
-	}
-	document.getElementById("htmlCode").value=htmlCode;
-	update();
+// 		if(htmlCode.length==0){
+// 			htmlCode+="<form>"+"\n"+"<input type='text'>"+"\n"+"<input type='submit'>"+"\n"+"</form>";
+// 		}
+// 		else{
+// 			htmlCode+="\n"+"<form>"+"\n"+"<input type='text'>"+"\n"+"<input type='submit'>"+"\n"+"</form>";
+// 		}
+// 	}
+// 	document.getElementById("htmlCode").value=htmlCode;
+// 	update(0);
 	
-}
+// }
 
 Split([".container", ".iframe-container"]);
 //Split(["#htmlCode", "#cssCode", "#javascriptCode"]);
