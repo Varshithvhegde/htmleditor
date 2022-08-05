375f747252bb94fe7595265792f611bafedf3807
Varshithvhegde <varshithvh@gmail.com>
2022-08-05 16:07:23 +0530

    Added auto closing tags

--- app.js
+++ app.js
@@ -11,5 +11,67 @@ function update() {
 	iframe.close();
 	
 }
+const closeChars = new Map([
+	['{', '}'],
+	['[', ']'],
+	['(', ')'],
+	['<','>'],
+	['"','"'],
+	["'","'"],
+	['<','>']
+  ]);
+	  
+  htmlCode=document.getElementById('htmlCode');
+  
+  htmlCode.addEventListener('input', function (e) {
+  
+	  const pos = e.target.selectionStart;
+	  const val = [...e.target.value];
+	  
+	  const char = val.slice(pos-1, pos)[0];
+	  const closeChar = closeChars.get(char);
+	  
+	  if (closeChar) {
+		val.splice(pos, 0, closeChar);
+		e.target.value = val.join('');
+		e.target.selectionEnd = pos;
+	  }
+  });
+
+  cssCode=document.getElementById('cssCode');
+  
+  cssCode.addEventListener('input', function (e) {
+  
+	  const pos = e.target.selectionStart;
+	  const val = [...e.target.value];
+	  
+	  const char = val.slice(pos-1, pos)[0];
+	  const closeChar = closeChars.get(char);
+	  
+	  if (closeChar) {
+		val.splice(pos, 0, closeChar);
+		e.target.value = val.join('');
+		e.target.selectionEnd = pos;
+	  }
+  });
+
+  javascriptCode=document.getElementById('javascriptCode');
+  
+  javascriptCode.addEventListener('input', function (e) {
+  
+	  const pos = e.target.selectionStart;
+	  const val = [...e.target.value];
+	  
+	  const char = val.slice(pos-1, pos)[0];
+	  const closeChar = closeChars.get(char);
+	  
+	  if (closeChar) {
+		val.splice(pos, 0, closeChar);
+		e.target.value = val.join('');
+		e.target.selectionEnd = pos;
+	  }
+  });
+
+
 Split([".container", ".iframe-container"]);
 //Split(["#htmlCode", "#cssCode", "#javascriptCode"]);
