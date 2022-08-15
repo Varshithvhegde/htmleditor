0caef8e833d1502522c11aa29551ae1b070cfa18
Varshithvhegde <varshithvh@gmail.com>
2022-08-15 20:13:59 +0530

    checking for coloring my tag

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ function update(i) {
 	let javascriptCode=document.getElementById("javascriptCode").value;
 	let text=htmlCode+"<style>"+cssCode+"</style>"+"<scri"+"pt>"+javascriptCode+"</scri"+"pt>";
 	let iframe=document.getElementById('viewer').contentWindow.document;
+	
 	iframe.open();
 	iframe.write(text);
 	iframe.close();
 	}
-	
+
 	else if(i==1){
 		
 		let htmlCode=document.getElementById("htmlCode").value;
